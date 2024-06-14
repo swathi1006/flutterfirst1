de07da0104f63bb196eca8c873af5a0b4819f718
swathi1006 <swathisahadevan996@gmail.com>
2024-06-14 21:47:55 +0530

    commit

--- lib/screens/splash_page_with_background.dart
+++ lib/screens/splash_page_with_background.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutterfirst/screens/login_page.dart';
 import 'package:flutterfirst/screens/ListViewww/listview using builder.dart';
